3dc58ab691ac8307d55b50816140527150930ddf
rbaker87 <rbaker87@gmail.com>
2012-09-20 19:40:20 +0000

    Added directory for terminal client to be stored for testing

---
+++ terminal_client/chatutils.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python
+import time
+import threading
+import signal
+import sys
+from beanstalk import serverconn
+from messages import *
+
+class CheckMessages(threading.Thread):
+    def __init__(self, conn):
+        threading.Thread.__init__(self)
+        self.conn = conn
+        self.user_length = 0
+        self.first_run = True
+        self.alive_users = []
+        self.beanserv = serverconn.ServerConn('localhost', 55833)
+
+    def presenceCB(self,conn,msg):
+        #Needs to work with logging in and off
+        #Currently only adds to the list
+        if str(msg.getFrom()) not in self.alive_users:
+            self.alive_users.append(str(msg.getFrom()))
+
+    def messageCB(self,conn,msg):
+        roster = self.conn.getRoster()
+        received_from = 'Blank'
+        for user in self.alive_users:
+            if str(user) == str(msg.getFrom()):
+                received_from = roster.getName(user)
+        self.beanserv.use('received')
+        self.beanserv.put("%s: %s" % (received_from, str(msg.getBody())))
+        sys.stdout.write("\n%s: %s\n\n" % (received_from, str(msg.getBody())))
+
+    def StepOn(self):
+        try:
+            self.conn.Process(1)
+            if self.first_run:
+                #Give the roster some time to populate correctly. Need a more elegant solution to this
+                time.sleep(2) 
+                self.first_run = False
+            roster = self.conn.getRoster()
+            roster_list = roster.getItems()
+
+            if self.user_length != len(self.alive_users):
+                self.beanserv.use('friends')
+                sys.stdout.write("***Friends List***\n")
+                for user in self.alive_users:
+                    if roster.getName(user) != None:
+                        self.beanserv.put(str(roster.getName(user)))
+                        sys.stdout.write("%s\n" % roster.getName(user))
+            self.user_length = len(self.alive_users)
+
+        except KeyboardInterrupt:
+            return 0
+        return 1
+
+    def run(self):
+        while self.StepOn():
+            pass
---
+++ terminal_client/main.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+import xmpp
+import sys
+import re
+import time
+from chatutils import CheckMessages
+from messages import *
+from beanstalk import serverconn
+
+try:
+    username = sys.argv[1]
+    passwd = 'AIR_%s' % sys.argv[2]
+    out_message = ''
+    beanserv = serverconn.ServerConn('localhost', 55833)
+except IndexError:
+    beanserv.use('errors')
+    beanserv.put(LOGIN_ERROR)
+    sys.exit("\nEnter a username and password\n")
+
+def main():
+    cl = xmpp.Client('pvp.net', debug=[])
+    if cl.connect(server=('chat.na1.lol.riotgames.com',5223)) == "":
+        beanserv.use('errors')
+        beanserv.put(CONN_ERROR)
+        sys.exit("Not connected\n")
+    if cl.auth(username,passwd,"xiff") == None:
+        beanserv.use('errors')
+        beanserv.put(AUTH_ERROR)
+        sys.exit("Authentication failed\n")
+    if cl.isConnected():
+        cl.sendInitPresence(requestRoster=1)
+
+        incoming_thread = CheckMessages(cl)
+        incoming_thread.setDaemon(True)
+        incoming_thread.start()
+
+        cl.RegisterHandler('presence', incoming_thread.presenceCB)
+        cl.RegisterHandler('message', incoming_thread.messageCB)
+
+        to_jid = None #jid for the user receiving the message
+        time.sleep(2.1) #Extremely inelegant solution to making the first prompt appear after the friends list populates. Fix later
+        while True:
+            out_message = raw_input()
+            out_message = re.split('(/\w+) (\w+)', out_message)
+            roster = cl.getRoster()
+            roster_list = roster.getItems()
+            try:
+                if out_message[1] == '/w':
+                    to_jid = out_message[2]
+                    out_message = out_message[3]
+            except IndexError:
+                try:
+                    if out_message[0] != '':
+                        out_message = out_message[0]
+                        try:
+                            to_jid = roster.getName(to_jid)
+                        except (KeyError, TypeError):
+                            to_jid = None
+                    else:
+                        out_message = ''
+                        to_jid = None
+                except IndexError:
+                    out_message = ''
+                    to_jid = None
+
+            if to_jid:
+                for user in incoming_thread.alive_users:
+                    if str(roster.getName(user)).lower() == str(to_jid).lower():
+                        to_jid = user
+                        valid_jid = True
+                        break
+                    else:
+                        valid_jid = False
+                if valid_jid:
+                    message = xmpp.Message(to_jid, out_message)
+                    message.setAttr('type', 'chat')
+                    cl.send(message)
+                else:
+                    beanserv.use('warnings')
+                    beanserv.put(USER_WARNING)
+                    sys.stderr.write("User not found...\n")
+    else:
+        beanserv.use('errors')
+        beanserv.put(CONN_ERROR)
+        sys.exit("Error connecting to server...\n")
+
+if __name__ == '__main__': 
+    try:
+        main()
+    except KeyboardInterrupt:
+        sys.stdout.write("\nLogging off...\n")
