9cf29eb82888ca4aeb2e4dd04b19b3aa77955a07
rbaker87 <rbaker87@gmail.com>
2012-09-26 16:58:59 +0000

    Added catch for an unexpected IOError that was causing apache to crap out. Moved messages to localizations for future use. Modified stream messages to use a key format to be identified and modified by the browser javascript.

--- README.md
+++ README.md
@@ -3,12 +3,16 @@ LeagueChat
 
 Intelligently interact with the League of Legends jaber network to send/receive messages without the Riot game client.
 
-Usage:
+Usage (terminal client):
 
     python main.py username password
+
+Usage (server):
+
+    Move localizations to your mod_pywebsocket install directory. Move leaguechat_wsh.py to your websocket handlers location. Restart apache.
 
 Requirements:
 
     xmpppy
 
-    beanstalkd
+    mod_pywebsocket
--- leaguechat_wsh.py
+++ leaguechat_wsh.py
@@ -3,7 +3,6 @@ import time
 import threading
 import signal
 import re
-from messages import *
 from mod_pywebsocket import msgutil
 
 class CheckMessages(threading.Thread):
@@ -28,7 +27,7 @@ class CheckMessages(threading.Thread):
         for user in self.alive_users:
             if str(user) == str(msg.getFrom()):
                 received_from = roster.getName(user)
-        self.message_sender.send_nowait("%s: %s" % (str(received_from), str(msg.getBody())))
+        self.message_sender.send_nowait("#:#message#:#%s: %s" % (str(received_from), str(msg.getBody())))
 
     def StepOn(self):
         try:
@@ -41,10 +40,10 @@ class CheckMessages(threading.Thread):
             roster_list = roster.getItems()
 
             if self.user_length != len(self.alive_users):
-                self.message_sender.send_nowait("***Friends List***")
+                self.message_sender.send_nowait("#:#clearfriends#:#")
                 for user in self.alive_users:
                     if roster.getName(user) != None:
-                        self.message_sender.send_nowait("%s" % roster.getName(user))
+                        self.message_sender.send_nowait("#:#friendupdate#:#%s" % roster.getName(user))
             self.user_length = len(self.alive_users)
 
         except KeyboardInterrupt:
@@ -60,72 +59,76 @@ def web_socket_do_extra_handshake(request):
 
 
 def web_socket_transfer_data(request):
-    line = request.ws_stream.receive_message()
-    if str(line).startswith('username'):
-        username = str(line)[8:]
-    line = request.ws_stream.receive_message()
-    if str(line).startswith('password'):
-        passwd = 'AIR_' + str(line)[8:]
-    out_message = ''
-    cl = xmpp.Client('pvp.net', debug=[])
-    if cl.connect(server=('chat.na1.lol.riotgames.com',5223)) == "":
-        request.ws_stream.send_message(CONN_ERROR, binary=False)
-    if cl.auth(username,passwd,"xiff") == None:
-        request.ws_stream.send_message(AUTH_ERROR, binary=False)
-    if cl.isConnected():
-        cl.sendInitPresence(requestRoster=1)
+    try:
+        from messages_en import *   #Eventually support other localizations
+        line = request.ws_stream.receive_message()
+        if str(line).startswith('username'):
+            username = str(line)[8:]
+        line = request.ws_stream.receive_message()
+        if str(line).startswith('password'):
+            passwd = 'AIR_' + str(line)[8:]
+        out_message = ''
+        cl = xmpp.Client('pvp.net', debug=[])
+        if cl.connect(server=('chat.na1.lol.riotgames.com',5223)) == "":
+            request.ws_stream.send_message(CONN_ERROR, binary=False)
+        if cl.auth(username,passwd,"xiff") == None:
+            request.ws_stream.send_message(AUTH_ERROR, binary=False)
+        if cl.isConnected():
+            cl.sendInitPresence(requestRoster=1)
 
-        message_sender = msgutil.MessageSender(request)
+            message_sender = msgutil.MessageSender(request)
 
-        incoming_thread = CheckMessages(cl, message_sender)
-        incoming_thread.setDaemon(True)
-        incoming_thread.start()
+            incoming_thread = CheckMessages(cl, message_sender)
+            incoming_thread.setDaemon(True)
+            incoming_thread.start()
 
-        cl.RegisterHandler('presence', incoming_thread.presenceCB)
-        cl.RegisterHandler('message', incoming_thread.messageCB)
+            cl.RegisterHandler('presence', incoming_thread.presenceCB)
+            cl.RegisterHandler('message', incoming_thread.messageCB)
 
-        to_jid = None #jid for the user receiving the message
-        time.sleep(2.1) #Extremely inelegant solution to making the first prompt appear after the friends list populates. Fix later
-        while True:
-            line = request.ws_stream.receive_message()
-            out_message = str(line)
-            out_message = re.split('(/\w+) (\w+)', out_message)
-            roster = cl.getRoster()
-            roster_list = roster.getItems()
-            try:
-                if out_message[1] == '/w':
-                    to_jid = out_message[2]
-                    out_message = out_message[3]
-            except IndexError:
-                try:
-                    if out_message[0] != '':
-                        out_message = out_message[0]
+            to_jid = None #jid for the user receiving the message
+            time.sleep(2.1) #Extremely inelegant solution to making the first prompt appear after the friends list populates. Fix later
+            while True:
+                line = request.ws_stream.receive_message()
+                out_message = str(line)
+                if out_message != "Keep alive":
+                    out_message = re.split('(/\w+) (\w+)', out_message)
+                    roster = cl.getRoster()
+                    roster_list = roster.getItems()
+                    try:
+                        if out_message[1] == '/w':
+                            to_jid = out_message[2]
+                            out_message = out_message[3]
+                    except IndexError:
                         try:
-                            to_jid = roster.getName(to_jid)
-                        except (KeyError, TypeError):
+                            if out_message[0] != '':
+                                out_message = out_message[0]
+                                try:
+                                    to_jid = roster.getName(to_jid)
+                                except (KeyError, TypeError):
+                                    to_jid = None
+                            else:
+                                out_message = ''
+                                to_jid = None
+                        except IndexError:
+                            out_message = ''
                             to_jid = None
-                    else:
-                        out_message = ''
-                        to_jid = None
-                except IndexError:
-                    out_message = ''
-                    to_jid = None
 
-            if to_jid:
-                for user in incoming_thread.alive_users:
-                    if str(roster.getName(user)).lower() == str(to_jid).lower():
-                        to_jid = user
-                        valid_jid = True
-                        break
-                    else:
-                        valid_jid = False
-                if valid_jid:
-                    message = xmpp.Message(to_jid, out_message)
-                    message.setAttr('type', 'chat')
-                    cl.send(message)
-                else:
-                    request.ws_stream.send_message(USER_WARNING, binary=False)
-    else:
-        request.ws_stream.send_message(CONN_ERROR, binary=False)
-
+                    if to_jid:
+                        for user in incoming_thread.alive_users:
+                            if str(roster.getName(user)).lower() == str(to_jid).lower():
+                                to_jid = user
+                                valid_jid = True
+                                break
+                            else:
+                                valid_jid = False
+                        if valid_jid:
+                            message = xmpp.Message(to_jid, out_message)
+                            message.setAttr('type', 'chat')
+                            cl.send(message)
+                        else:
+                            request.ws_stream.send_message(USER_WARNING, binary=False)
+        else:
+            request.ws_stream.send_message(CONN_ERROR, binary=False)
+    except IOError: #Something was causing apache to overload... Meh?
+        return
 # vi:sts=4 sw=4 et
